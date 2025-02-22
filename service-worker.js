dc1e1acc5eedd4c2cfe7f8c789d1e9ca90a4a9ef
Nerothon <nerothon.official@gmail.com>
2025-02-22 21:22:01 +0100

    Update build

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: qXAJegg5 */
+/* Manifest version: jJA/A3el */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
